8200e792e7c83aa2f44694f957672f613ac490f5
robertovilla2102 <robertovilla2102@gmail.com>
2020-01-22 17:24:23 -0300

    retoques finales

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
+// Todos los require para tener las librerias activas
 const express = require('express')
 const nunjucks = require('nunjucks')
 const morgan = require('morgan')
 const app = express()
 
-//usando nunjucks
+//usando nunjucks, para que reconozca los html
 app.engine('html',nunjucks.render)
 app.set(' view engine' , 'html') // seteamos el view engine como html
 nunjucks.configure('views', {noCache: true});
 
-
 // utlizimaos esto para parsear el http
 app.use(express.urlencoded({extended : false}))
 
+// 1er Middlewar, tambien utilizamos morgan
 app.use(morgan('dev'))
-
 app.use(function (req, res, next) {
     console.log(req.method, req.originalUrl)
-//    console.log(req)
     next()
 })
 
-
-// segundo middlewar
-app.use( '/special', function(req,res,next){
-    
-})
-
-
-
-
-/* RUTAS */
+// Rutas 
 app.use(require('./routes/index'))
 
 
 /* STATICS FILES */
 app.use(express.static('./public'))
 
-// servidor
+// servidor montado
 app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), () =>{
     console.log('El servido ya anda!' , app.get('port'))
--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,32 @@ const express = require('express')
 const tweetBank = require('../tweetBank');
 const router = express.Router()
 
+// creamos la pagina principal, usando la funcion list() e=de tweetBank
 router.get('/', (req,res) =>{
     let tweet = tweetBank.list();
     res.render('index.html',{ tweets: tweet, showform:true})
 })
 
-// pagina para agregar tweets
+// pagina para agregar  nuevos tweets, usando la funcion add() de tweetyBank
  router.post('/agregarTweet' , (req,res) =>{
     let {name, content} = req.body
     tweetBank.add(name, content)
     res.redirect('/')
 } ) 
 
-// ruta para cada usuario
+// ruta para cada twitt, solo para que aparezca ese
 router.get('/tweet/:id', (req,res) => {
     let id = parseInt(req.params.id)
     let tweetsUser = tweetBank.find({id : id})
     res.render('index.html',{tweets : tweetsUser})
 })
 
+// ruta para todos los ttwitss de la misma persona
 router.get('/users/:name', function(req, res) {
     var name = req.params.name;
     var tweet = tweetBank.find( { name: name } );
     res.render( 'index.html', { tweets: tweet,showform : false } );
   });
 
+// esto no me acuerdo de que es
 module.exports = router;
